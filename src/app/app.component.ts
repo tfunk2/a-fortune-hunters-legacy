7344f22e14b91fa07a11cac93684994e6dae4b78
tfunk2 <tyler.j.funk2@gmail.com>
2021-04-13 09:42:30 -0600

    Added music symbol imgs to the screen of the keypad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,29 @@ export class AppComponent {
       this.currentComboAttempt = [];
     }
   }
+
+  whichMusicNote(currentNum) {
+    switch(currentNum) {
+      case 1:
+        return "../assets/music-note-pngs/bass-clef.png";
+      case 2:
+        return "../assets/music-note-pngs/beam.png";
+      case 3:
+        return "../assets/music-note-pngs/eighth-note.png";
+      case 4:
+        return "../assets/music-note-pngs/half-note.png";
+      case 5:
+        return "../assets/music-note-pngs/quarter-note-rest.png";
+      case 6:
+        return "../assets/music-note-pngs/quaver.png";
+      case 7:
+        return "../assets/music-note-pngs/sixteenth-note.png";
+      case 8:
+        return "../assets/music-note-pngs/tie.png";
+      case 9:
+        return "../assets/music-note-pngs/triplet.png";
+      case 0:
+        return "../assets/music-note-pngs/treble-clef.png";
+    }
+  }
 }
