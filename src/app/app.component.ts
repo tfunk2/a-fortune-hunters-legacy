bcafc1f26d3cef82abdd29e7a31a7fab4b616f0c
tfunk2 <tyler.j.funk2@gmail.com>
2021-04-13 06:58:35 -0600

    Initial creation of working keypad with minimal styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'a-fortune-hunters-legacy';
+  title: string = "A Fortune Hunter's Legacy";
+  isKeypadLocked: boolean = true;
+  correctCombination: number[] = [2, 4, 3, 6];
+  currentComboAttempt: number[] = [];
+
+  addSymbolToCombo(num) {
+    if(this.currentComboAttempt.length <= 3) {
+      this.currentComboAttempt = [...this.currentComboAttempt, num];
+    }
+  }
+
+  submitCombo() {
+    if(this.currentComboAttempt.join("") === this.correctCombination.join("")) {
+      this.isKeypadLocked = false;
+    } else {
+      this.currentComboAttempt = [];
+    }
+  }
 }
