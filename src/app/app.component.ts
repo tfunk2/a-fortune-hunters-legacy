ba40c13a4c21274a11d0eab529bcc8cbab95537b
tfunk2 <tyler.j.funk2@gmail.com>
2021-04-16 08:09:06 -0600

    Added alien letters and styled in a grid, ready for initial deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = "The Legacy Continues...";
+  title: string = 'The Legacy Continues...';
   isKeypadLocked: boolean = true;
   isKeypadBeingUnlocked: boolean = false;
   correctCombination: number[] = [0, 7, 4, 2];
   currentComboAttempt: number[] = [];
+  alieneseImagePaths: string[] = [
+    '../assets/alienese/a.png',
+    '../assets/alienese/b.png',
+    '../assets/alienese/c.png',
+    '../assets/alienese/d.png',
+    '../assets/alienese/e.png',
+    '../assets/alienese/f.png',
+    '../assets/alienese/g.png',
+    '../assets/alienese/h.png',
+    '../assets/alienese/i.png',
+    '../assets/alienese/j.png',
+    '../assets/alienese/k.png',
+    '../assets/alienese/l.png',
+    '../assets/alienese/m.png',
+    '../assets/alienese/n.png',
+    '../assets/alienese/o.png',
+    '../assets/alienese/p.png',
+    '../assets/alienese/q.png',
+    '../assets/alienese/r.png',
+    '../assets/alienese/s.png',
+    '../assets/alienese/t.png',
+    '../assets/alienese/u.png',
+    '../assets/alienese/v.png',
+    '../assets/alienese/w.png',
+    '../assets/alienese/x.png',
+    '../assets/alienese/y.png',
+    '../assets/alienese/z.png',
+  ];
+  alphabet: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
 
   addSymbolToCombo(num) {
-    if(this.currentComboAttempt.length <= 3) {
+    if (this.currentComboAttempt.length <= 3) {
       this.currentComboAttempt = [...this.currentComboAttempt, num];
     }
   }
 
   clearCombo() {
-    this.currentComboAttempt = []
+    this.currentComboAttempt = [];
   }
 
- submitCombo() {
-    if(this.currentComboAttempt.join("") === this.correctCombination.join("")) {
+  submitCombo() {
+    if (
+      this.currentComboAttempt.join('') === this.correctCombination.join('')
+    ) {
       this.isKeypadBeingUnlocked = true;
       this.currentComboAttempt = [];
-      setTimeout(() => {
-        
-      }, 1000);
+      setTimeout(() => {}, 1000);
       setTimeout(() => {
         this.isKeypadLocked = false;
       }, 2000);
@@ -38,27 +94,84 @@ export class AppComponent {
   }
 
   whichMusicNote(currentNum) {
-    switch(currentNum) {
+    switch (currentNum) {
       case 1:
-        return "../assets/music-note-pngs/bass-clef.png";
+        return '../assets/music-note-pngs/bass-clef.png';
       case 2:
-        return "../assets/music-note-pngs/beam.png";
+        return '../assets/music-note-pngs/beam.png';
       case 3:
-        return "../assets/music-note-pngs/eighth-note.png";
+        return '../assets/music-note-pngs/eighth-note.png';
       case 4:
-        return "../assets/music-note-pngs/half-note.png";
+        return '../assets/music-note-pngs/half-note.png';
       case 5:
-        return "../assets/music-note-pngs/quarter-note-rest.png";
+        return '../assets/music-note-pngs/quarter-note-rest.png';
       case 6:
-        return "../assets/music-note-pngs/quaver.png";
+        return '../assets/music-note-pngs/quaver.png';
       case 7:
-        return "../assets/music-note-pngs/sixteenth-note.png";
+        return '../assets/music-note-pngs/sixteenth-note.png';
       case 8:
-        return "../assets/music-note-pngs/tie.png";
+        return '../assets/music-note-pngs/tie.png';
       case 9:
-        return "../assets/music-note-pngs/triplet.png";
+        return '../assets/music-note-pngs/triplet.png';
       case 0:
-        return "../assets/music-note-pngs/treble-clef.png";
+        return '../assets/music-note-pngs/treble-clef.png';
+    }
+  }
+
+  whichLetter(currentImagePath) {
+    switch (currentImagePath) {
+      case '../assets/alienese/a.png':
+        return 'a';
+      case '../assets/alienese/b.png':
+        return 'b';
+      case '../assets/alienese/c.png':
+        return 'c';
+      case '../assets/alienese/d.png':
+        return 'd';
+      case '../assets/alienese/e.png':
+        return 'e';
+      case '../assets/alienese/f.png':
+        return 'f';
+      case '../assets/alienese/g.png':
+        return 'g';
+      case '../assets/alienese/h.png':
+        return 'h';
+      case '../assets/alienese/i.png':
+        return 'i';
+      case '../assets/alienese/j.png':
+        return 'j';
+      case '../assets/alienese/k.png':
+        return 'k';
+      case '../assets/alienese/l.png':
+        return 'l';
+      case '../assets/alienese/m.png':
+        return 'm';
+      case '../assets/alienese/n.png':
+        return 'n';
+      case '../assets/alienese/o.png':
+        return 'o';
+      case '../assets/alienese/p.png':
+        return 'p';
+      case '../assets/alienese/q.png':
+        return 'q';
+      case '../assets/alienese/r.png':
+        return 'r';
+      case '../assets/alienese/s.png':
+        return 's';
+      case '../assets/alienese/t.png':
+        return 't';
+      case '../assets/alienese/u.png':
+        return 'u';
+      case '../assets/alienese/v.png':
+        return 'v';
+      case '../assets/alienese/w.png':
+        return 'w';
+      case '../assets/alienese/x.png':
+        return 'x';
+      case '../assets/alienese/y.png':
+        return 'y';
+      case '../assets/alienese/z.png':
+        return 'z';
     }
   }
 }
